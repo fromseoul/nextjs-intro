512166cbdef96b0b9b492097604ada6cc4a4ffe2
fromseoul <jmkang@goqual.com>
2022-07-31 14:08:09 +0900

    css modules

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from './NavBar.module.css';
 
 export default function NavBar() {
 
@@ -10,10 +11,10 @@ export default function NavBar() {
   return (
     <nav>
       <Link href="/">
-        <a>Home</a>
+        <a className={`${styles.link} ${router.pathname === '/' ? styles.active : ''}`}>Home</a>
       </Link>
       <Link href="/about">
-        <a>About</a>
+        <a className={[styles.link, router.pathname === '/about' ? styles.active  : ''].join(" ")}>About</a>
       </Link>
     </nav>
   )
