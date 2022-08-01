beb9793a81ecdb3b9d71fa4438c8151534485a76
fromseoul <jmkang@goqual.com>
2022-08-01 20:41:26 +0900

    router detail

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Seo from "../components/Seo";
 
 type Props = {
   results: any;
-}
+};
 
 export default function Home({ results }: Props) {
   const [movies, setMovies] = useState([]);
@@ -21,7 +21,7 @@ export default function Home({ results }: Props) {
     <div className="container">
       <Seo title="Home" />
       {/*{!movies && <h4>Loading...</h4>}*/}
-      {results .map((movie: any) => (
+      {results.map((movie: any) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -34,15 +34,22 @@ export default function Home({ results }: Props) {
           padding: 20px;
           gap: 20px;
         }
+
+        .movie {
+          cursor: pointer;
+        }
+
         .movie img {
           max-width: 100%;
           border-radius: 12px;
           transition: transform 0.2s ease-in-out;
           box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
         }
+
         .movie:hover img {
           transform: scale(1.05) translateY(-10px);
         }
+
         .movie h4 {
           font-size: 18px;
           text-align: center;
@@ -52,11 +59,13 @@ export default function Home({ results }: Props) {
   );
 }
 
-export async function getServerSideProps () {
-  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
   return {
     props: {
-      results
-    }
-  }
+      results,
+    },
+  };
 }
