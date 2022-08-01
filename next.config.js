f335e9f2f058b87fd6f10c1a64d1cda45b34ec39
fromseoul <jmkang@goqual.com>
2022-08-01 20:02:19 +0900

    rewrite and hide env on next.config.js

--- next.config.js
+++ next.config.js
@@ -8,6 +8,20 @@ const nextConfig = {
       destination: '/form',
       permanent: false,
     }]
+    // return [{
+    //   source: '/old-blog/:path*',
+    //   destination: '/new-blog/:path*',
+    //   permanent: false,
+    // }]
+
+  },
+  async rewrites () {
+    return [
+      {
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
+      }
+    ]
   }
 }
 
